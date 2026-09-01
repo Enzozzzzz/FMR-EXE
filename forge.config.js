e83f6ecd17a2762adcf55752f4dcc6f0b0c23ef1
Enzoglr <Enzo23025@gmail.com>
2026-09-01 18:05:05 +0200

    workflow

--- forge.config.js
+++ forge.config.js
@@ -9,7 +9,9 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        name: 'FMR'
+      },
     },
     {
       name: '@electron-forge/maker-zip',
